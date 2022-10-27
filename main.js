19e44181a0043cb2e1b661c0cb58cd82baff38be
Jocabed Ríos <j0c4b3d@gmail.com>
2022-10-26 23:14:36 -0500

    At the moment the program can jump and validate that the jump's length don't exit the limits (is negative or is bigger than the number of instructions).

--- main.js
+++ main.js
@@ -1,22 +1,50 @@
-if (process.argv.length < 3) {
-    console.log('Usage: node ' + process.argv[1] + ' FILENAME');
-    process.exit(1);
-  }
-  
-  var fs = require('fs')
+var a = process.argv[3];
+var b = process.argv[4];
+
+var fs = require('fs')
     , filename = process.argv[2];
-  fs.readFile(filename, 'utf8', function(err, data) {
+fs.readFile(filename, 'utf8', function(err, data) {
     if(err){
       console.error(err);
       return;
     }
-    lineByLine(data);
-  });
-  
-  function lineByLine(text){
-      var e1, e2, e3;
-      var lines = text.split(/[\r\n]+/g);
-      for(var i = 0; i < lines.length; i++) {
-          console.log(lines[i]);
-      }
-  }
+    console.log("a = "+a+",b = "+b)
+    var lines = data.split(/[\r\n]+/g);
+    analyzer(lines, 0);
+    
+ });
+
+function analyzer(text, count){
+    if(count<(text.length-1)){
+        console.log(count+". "+text[count]);
+        var words = text[count].split(" ");
+        var instruction = words[0];
+        var offset;
+        var aux;
+
+        if(instruction[0]==="j"){
+            offset = words[2];
+            aux = jump(count, offset, text.length-1);
+        }else {
+            aux = count+1;
+        }
+
+        //console.log(aux);
+        analyzer(text, aux);
+
+    
+    }
+    else console.log("Program finished. \na = "+a+" b = "+b);
+    
+}
+
+function jump(count, offset, limit){
+    var longitudJump;
+    var direction = offset.charAt(0)
+    var longitud = parseInt(offset.substring(1,offset.length));
+
+    if(direction === '+') longitudJump = count + longitud;
+    else longitudJump = count -longitud;
+    if(longitudJump>limit || longitudJump<0) longitudJump = limit;
+    return longitudJump;
+}
